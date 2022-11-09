ddee226a8d0451899a9013eeff29b203c49285a7
Timo Heller <53556383+timoheller99@users.noreply.github.com>
2022-11-09 17:21:41 +0100

    add data access validation

---
+++ src/LayerArchitectureExample.DataAccess.Validation/Core/GuidValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace LayerArchitectureExample.DataAccess.Validation.Core;
+
+using System;
+
+using FluentValidation;
+
+public class GuidValidator : AbstractValidator<Guid>
+{
+    public GuidValidator()
+    {
+        this.RuleFor(x => x)
+            .NotEqual(Guid.Empty)
+            .OverridePropertyName("Id");
+    }
+}
---
+++ src/LayerArchitectureExample.DataAccess.Validation/Core/NameValidator.cs
@@ -0,0 +1,19 @@
+﻿namespace LayerArchitectureExample.DataAccess.Validation.Core;
+
+using FluentValidation;
+
+public class NameValidator : AbstractValidator<string>
+{
+    public NameValidator()
+    {
+        this.RuleFor(m => m)
+            .Must(m => !string.IsNullOrWhiteSpace(m));
+    }
+
+    protected override void EnsureInstanceNotNull(object instanceToValidate)
+    {
+        // It's possible that instance that should be validated is null (default(string)).
+        // Normally this would cause an ArgumentNullException thrown by FluentValidations Guard.
+        // By overriding this method, it's possible to pass null as the instance to validate.
+    }
+}
