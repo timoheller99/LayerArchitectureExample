6c7e265129ca4231dd6adeacdc55d4a3ff60b7e5
Timo Heller <53556383+timoheller99@users.noreply.github.com>
2022-11-09 17:05:27 +0100

    add validation classes

---
+++ src/LayerArchitectureExample.Core.Validation/Contracts/IValidationService.cs
@@ -0,0 +1,10 @@
+﻿namespace LayerArchitectureExample.Core.Validation.Contracts;
+
+using System.Threading.Tasks;
+
+using FluentValidation.Results;
+
+public interface IValidationService<TValidationModel>
+{
+    public Task<ValidationResult> ValidateAsync(TValidationModel model);
+}
