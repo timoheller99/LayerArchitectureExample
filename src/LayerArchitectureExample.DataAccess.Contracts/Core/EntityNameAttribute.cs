95c42f7fcc8bc250692ee7cd02c74f1457e4ffea
Timo Heller <53556383+timoheller99@users.noreply.github.com>
2023-01-12 21:05:47 +0100

    add custom attribute and method to retrieve EntityName

---
+++ src/LayerArchitectureExample.DataAccess.Contracts/Core/EntityNameAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace LayerArchitectureExample.DataAccess.Contracts.Core;
+
+using System;
+
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class EntityNameAttribute : Attribute
+{
+    public EntityNameAttribute(string entityName)
+    {
+        this.EntityName = entityName ?? throw new ArgumentNullException(nameof(entityName));
+    }
+
+    public string EntityName { get; }
+}
--- src/LayerArchitectureExample.DataAccess.Contracts/Core/IDbModel.cs
+++ src/LayerArchitectureExample.DataAccess.Contracts/Core/IDbModel.cs
@@ -1,6 +1,21 @@
 ﻿namespace LayerArchitectureExample.DataAccess.Contracts.Core;
 
+using System;
+using System.Data;
+
 public interface IDbModel<TIdType>
 {
     public TIdType Id { get; set; }
+
+    public string GetEntityName()
+    {
+        var attribute = (EntityNameAttribute)Attribute.GetCustomAttribute(this.GetType(), typeof(EntityNameAttribute));
+
+        if (attribute == null)
+        {
+            throw new ConstraintException($"Type {this.GetType()} does not have an {nameof(EntityNameAttribute)} attribute declared.");
+        }
+
+        return attribute.EntityName;
+    }
 }
